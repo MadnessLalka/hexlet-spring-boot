c0ba936f2fcc2aa8738df38f5e82d6e8aa188874
ilya.fedorov <scilock@yandex.ru>
2026-07-02 17:07:47 +0300

    initial commit

---
+++ src/test/java/ru/ml/hexlet/HexletSpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ml.hexlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexletSpringBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
