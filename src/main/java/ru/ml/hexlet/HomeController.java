9ffbac532b57f6d1c49f99ff4f6ea27d1873a081
ilya.fedorov <scilock@yandex.ru>
2026-07-02 17:55:14 +0300

    feat: add GET endpoint "/about"

--- src/main/java/ru/ml/hexlet/HomeController.java
+++ src/main/java/ru/ml/hexlet/HomeController.java
@@ -10,4 +10,12 @@ public class HomeController {
     public String home() {
         return "Добро пожаловать в Hexlet Spring Blog!";
     }
+
+    @GetMapping("/about")
+    public String about() {
+        return """
+                <h1>Привет читатель!</h1>
+                <p>Это тестовый проект для изучения Spring Boot на Hexlet!</p>
+                """;
+    }
 }
